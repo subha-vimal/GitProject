0887cdb3b69014c75b3dbdc22078c8913f771dcb
subha-vimal <subhashiniramesh23.93@gmail.com>
2024-03-11 17:18:27 +0530

    dhivya

--- src/test/java/org/runner/TestRunner.java
+++ src/test/java/org/runner/TestRunner.java
@@ -7,12 +7,8 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="D:\\GitHub\\CucumberFacebook\\GitProject\\src\\test\\resources\\Facebook\\Facebook.feature",glue="org.stepDefinition",
-<<<<<<< HEAD
-dryRun = false,plugin= {"json:D:\\GitHub\\CucumberFacebook\\GitProject\\target\\Reports\\fb.json"})
-=======
-dryRun = false,plugin= {"html:D:\\GitHub\\CucumberFacebook\\GitProject\\target\\Reports\\fb","junit:D:\\GitHub\\CucumberFacebook\\GitProject\\target\\Reports\\fb.xml"}
+dryRun = false,plugin= {"html:D:\\GitHub\\CucumberFacebook\\GitProject\\target\\Reports\\fb","junit:D:\\GitHub\\CucumberFacebook\\GitProject\\target\\Reports\\fb.xml","json:D:\\GitHub\\CucumberFacebook\\GitProject\\target\\Reports\\fb.json"}
 ,tags="@sanity,@smoke")
->>>>>>> d925b8accb4e648a94780465f7cfcf95314d2ec4
 public class TestRunner {
 
 }
